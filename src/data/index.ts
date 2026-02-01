94130f4b52ae0958bc323540c06dcc13a9cd0a29
Sebastian <jchingapalacios05@gmail.com>
2026-01-31 21:21:59 -0500

    chore: add projects in array

--- src/data/index.ts
+++ src/data/index.ts
@@ -11,5 +11,36 @@ export const tecnologiasData: TecnologiasType[] = [
 ]
 
 export const proyectosData: ProyectosType[] = [
-    { id: 1, titulo: 'Gestor de tareas', descripcion: 'Sistema para gestionar tareas', imagen: 'admin-tareas', repositorio: 'www.facebook.com', url: 'php.net' }
+    {
+        id: 1,
+        titulo: 'Administrador de tareas',
+        descripcion: 'Aplicación para crear, organizar y filtrar tareas por estado, ayudando a mantener el control del trabajo diario.',
+        imagen: 'admin-tareas',
+        repositorio: 'https://github.com/sebastianchinga/task-manager-ts-',
+        url: 'https://task-zend.netlify.app/'
+    },
+    {
+        id: 2,
+        titulo: 'Facturacion de ventas',
+        descripcion: 'Sistema de facturación que registra ventas, clientes y totales, con generación de comprobantes y control básico.',
+        imagen: 'facturacion-ventas',
+        repositorio: 'https://github.com/sebastianchinga/inventario-ventas',
+        url: 'https://task-zend.netlify.app/'
+    },
+    {
+        id: 3,
+        titulo: 'Página de bienes raíces',
+        descripcion: 'Sitio web inmobiliario para mostrar casas y departamentos, con diseño moderno y enfoque en propiedades de lujo.',
+        imagen: 'bienes-raices',
+        repositorio: 'https://github.com/sebastianchinga/inventario-ventas',
+        url: 'https://task-zend.netlify.app/'
+    },
+    {
+        id: 3,
+        titulo: 'Consejos Randoms',
+        descripcion: 'Aplicación que muestra consejos aleatorios en tiempo real con una interfaz simple, oscura y agradable al usuario.',
+        imagen: 'consejos-randoms',
+        repositorio: 'https://github.com/sebastianchinga/inventario-ventas',
+        url: 'https://task-zend.netlify.app/'
+    },
 ]
